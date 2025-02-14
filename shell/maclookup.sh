f876444e49e54fd881997870571011b2c1023368
iamenderst <enderst@gmail.com>
2025-02-14 15:01:52 -0700

    Update maclookup.sh

--- shell/maclookup.sh
+++ shell/maclookup.sh
@@ -3,6 +3,8 @@
 if [ "$1" == "" ]; then
     echo "Usage: $0 <MAC Address>"
     echo "Example: $0 00:0a:95:9d:68:16"
+    echo "Example: $0 00:0A:95:9D:68:16"
+    echo "Example: $0 00:0a.959d.6816"
     exit 1
 fi
 echo -ne $1 - `wget -qO- "https://api.macaddress.io/v1?apiKey=YOUR_API_KEY_HERE&output=vendor&search=$1"`
